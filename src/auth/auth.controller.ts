0095507166d4d5110f07e2c8322dda0717689bea
chdespon <chdespon@student.42.fr>
2023-01-31 20:13:51 +0100

    first step of learning nestJS

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Post, Body} from "@nestjs/common";
+import { AuthService } from "./auth.service";
+import { AuthDto } from "./dto";
+
+@Controller('auth')
+export class AuthControler {
+	constructor(private authService: AuthService) {}
+
+	@Post('signup')
+	signup(@Body() dto: AuthDto) {
+		return this.authService.signup()}
+
+	@Post('signin')
+	signin() {return this.authService.signin()}
+
+}
