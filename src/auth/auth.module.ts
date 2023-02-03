2f20fc466e8d1aa72a2b9054cacdacd31c753c8a
chdespon <chdespon@student.42.fr>
2023-02-03 18:29:36 +0100

    remove files

--- src/auth/auth.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from "@nestjs/common";
-import { AuthControler } from "./auth.controller";
-import { AuthService } from "./auth.service";
-
-@Module({
-	controllers: [AuthControler],
-	providers: [AuthService],
-})
-export class AuthModule {}
