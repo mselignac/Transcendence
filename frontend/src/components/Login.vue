48b38a5246f16ef7935aa7642e105e942fd5f4c7
Elisa JAHAN <ejahan@bess-f3r1s5.clusters.42paris.fr>
2023-06-19 16:26:11 +0200

    front

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -10,4 +10,9 @@ export default {
   <div className="main_login_div">
     <RouterLink to="/login" className="button_test">Login</RouterLink>
   </div>
+  <!-- <div class="center">
+  <div class="btn-1">
+    <a href=""><span>Login</span></a>
+  </div> -->
+<!-- </div> -->
 </template>
